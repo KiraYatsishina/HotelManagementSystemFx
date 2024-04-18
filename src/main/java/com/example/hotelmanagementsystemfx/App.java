92a3a1a5000d045cb1a62182ffbe808be0c4aef9
Kira Yatsishina <yatsishinakira@gmail.com>
2024-04-18 23:19:00 +0300

    add photo and styles to Login.fxml

--- src/main/java/com/example/hotelmanagementsystemfx/App.java
+++ src/main/java/com/example/hotelmanagementsystemfx/App.java
@@ -4,6 +4,7 @@ import com.example.hotelmanagementsystemfx.DB.DatabaseHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -11,9 +12,11 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Fxml/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Hotel Management System");
         stage.setScene(scene);
+        Image icon = new Image("C:\\Users\\kirus\\IdeaProjects\\HotelManagementSystemFx\\src\\main\\resources\\com\\example\\hotelmanagementsystemfx\\Images\\hotelIcon.png");
+        stage.getIcons().add(icon);
         stage.show();
     }
 
