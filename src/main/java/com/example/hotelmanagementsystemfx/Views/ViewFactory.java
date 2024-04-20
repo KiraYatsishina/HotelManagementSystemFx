6092d6b08be087a91c3e08d40593252688b72253
Kira Yatsishina <yatsishinakira@gmail.com>
2024-04-20 20:24:22 +0300

    finish ManagerMenu.fxml

--- src/main/java/com/example/hotelmanagementsystemfx/Views/ViewFactory.java
+++ src/main/java/com/example/hotelmanagementsystemfx/Views/ViewFactory.java
@@ -29,18 +29,7 @@ public class ViewFactory {
     public void showLoginWindow(){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/hotelmanagementsystemfx/Fxml/Login.fxml"));
         Scene scene = null;
-        try{
-            scene = new Scene(loader.load());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        Stage stage = new Stage();
-        stage.setTitle("Hotel Management System");
-        stage.setScene(scene);
-        stage.setResizable(false);
-        Image icon = new Image("C:\\Users\\kirus\\IdeaProjects\\HotelManagementSystemFx\\src\\main\\resources\\com\\example\\hotelmanagementsystemfx\\Images\\hotelIcon.png");
-        stage.getIcons().add(icon);
-        stage.show();
+        createStage(loader);
     }
 
     public void showManagerWindow(){
@@ -58,12 +47,8 @@ public class ViewFactory {
         }
         Stage stage = new Stage();
         stage.setTitle("Hotel Management System");
+        stage.setResizable(false);
         stage.setScene(scene);
-//        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-//        stage.setX(primaryScreenBounds.getMinX());
-//        stage.setY(primaryScreenBounds.getMinY());
-//        stage.setWidth(primaryScreenBounds.getWidth());
-//        stage.setHeight(primaryScreenBounds.getHeight());
         Image icon = new Image("C:\\Users\\kirus\\IdeaProjects\\HotelManagementSystemFx\\src\\main\\resources\\com\\example\\hotelmanagementsystemfx\\Images\\hotelIcon.png");
         stage.getIcons().add(icon);
         stage.show();
